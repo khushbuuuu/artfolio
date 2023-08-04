46b371f931855cb64f72a3f850927a2dea4d53a3
Khushboo <ramanikjm@gmail.com>
2023-08-05 00:15:02 +0530

    added react router config

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import "./App.css";
+import { NavBarog } from "./components/NavBarog";
+import { Banner } from "./components/Banner";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Aboutme } from "./components/Aboutme";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css';
-import { NavBarog } from './components/NavBarog';
-import { Banner } from './components/Banner';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
-import {Footer} from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Aboutme } from './components/Aboutme';
+const HomePage = () => {
+  return (
+    <div className="App">
+      <NavBarog />
+      <Banner />
+      <Projects />
+      <Aboutme />
+      <Contact />
 
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      
-      <NavBarog/>
-      <Banner/>
-      <Projects/>
-      <Aboutme/>
-      <Contact/>
-     
-      <Footer/>
-     
-    </div>
+    <Routes>
+      <Route index path="/" element={<HomePage />} />
+      <Route path="/something" element={<div> New Page </div>} />
+    </Routes>
   );
 }
 
